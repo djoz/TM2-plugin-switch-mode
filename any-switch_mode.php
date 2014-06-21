ce1222c7b3837e5f2d466084a9eda810406c63bf
doe-eye <doe-eye@gmx.net>
2014-06-21 03:10:14 +0200

    Update any-switch_mode.php

--- any-switch_mode.php
+++ any-switch_mode.php
@@ -1,18 +1,1 @@
-<?php
-Aseco::registerEvent('onEndMap', 'sm_endMap');
-
-
-function sm_endMap($aseco) {
-
-$gamemode = $aseco->client->query('GetGameMode()');
-
-if($gamemode == 2){//TA
-$aseco->client->query('SetGameMode(1)');
-$aseco->client->query('RestartMap');
-}
-else if($gamemode == 1){//Rounds
-$aseco->client->query('SetGameMode(2)');
-}
-
-
-?>
+.
